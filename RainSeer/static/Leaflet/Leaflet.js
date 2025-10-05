bd699409b87fd71a5364ff632ae10dbfa2852dca
diegoortega <yeyo082006@hotmail.com>
2025-10-05 10:33:15 -0700

    Commit RainSeer

---
+++ RainSeer/static/Leaflet/Leaflet.js
@@ -0,0 +1,15 @@
+var map = L.map('map').setView([20, 0], 2);
+
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([51.5, -0.09]).addTo(map);
+
+function onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+}
+
+map.on('click', onMapClick);
